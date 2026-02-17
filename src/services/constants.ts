092d62b141eea56e06cc57a24cdf588c5be846b7
SOFIRITARI AMGBARA <sofiriamgbara@gmail.com>
2026-02-17 17:47:15 +0000

    Update API URL in .env file and refactor code formatting in multiple files for consistency

--- src/services/constants.ts
+++ src/services/constants.ts
@@ -1,3 +1,3 @@
 export const _constants = {
-	API_URL: import.meta.env.VITE_API_URL,
+  API_URL: import.meta.env.VITE_API_URL,
 };
--- vite.config.ts
+++ vite.config.ts
@@ -4,25 +4,10 @@ import tailwindcss from "@tailwindcss/vite";
 import path from "path";
 
 export default defineConfig({
-	plugins: [tailwindcss(), react()],
-	resolve: {
-		alias: {
-			"@": path.resolve(__dirname, "./src"),
-		},
-	},
-	server: {
-		proxy: {
-			"/api": {
-				target: "https://kpoikpoimingi-backend-production.up.railway.app",
-				changeOrigin: true,
-				secure: false,
-				rewrite: (path) => path.replace(/^\/api/, ""),
-			},
-			"/auth": {
-				target: "https://kpoikpoimingi-backend-production.up.railway.app",
-				changeOrigin: true,
-				secure: false,
-			},
-		},
-	},
+  plugins: [tailwindcss(), react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 });
